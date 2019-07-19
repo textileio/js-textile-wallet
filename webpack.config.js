54613fc6e12e5a6a927f2750fd3dd58270d31c7e
Carson Farmer <carson.farmer@gmail.com>
2019-07-18 22:43:13 -0700

    chore: lint fixes
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path = require('path')
+const path = require("path");
 
-const extensions = ['.tsx', '.ts', '.js', 'json']
+const extensions = [".tsx", ".ts", ".js", "json"];
 
 module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.ts?$/,
-        include: path.resolve(__dirname, 'src'),
-        loader: 'ts-loader?configFile=tsconfig.webpack.json'
+        include: path.resolve(__dirname, "src"),
+        loader: "ts-loader?configFile=tsconfig.webpack.json"
       }
     ]
   },
@@ -18,9 +18,9 @@ module.exports = {
     extensions
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    library: 'wallet',
-    libraryTarget: 'var'
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    library: "wallet",
+    libraryTarget: "var"
   }
-}
+};
