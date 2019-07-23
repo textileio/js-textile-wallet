8d958d960f3ee3c39333598e4cd8c307f60dc849
Carson Farmer <carson.farmer@gmail.com>
2019-07-22 22:22:56 -0700

    fix: add webpack config to ignore list
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
-const path = require("path");
+const path = require('path')
 
-const extensions = [".tsx", ".ts", ".js", "json"];
+const extensions = ['.tsx', '.ts', '.js', 'json']
 
 module.exports = {
-  entry: "./src/index.ts",
-  devtool: "inline-source-map",
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
         test: /\.ts?$/,
-        include: path.resolve(__dirname, "src"),
-        loader: "ts-loader?configFile=tsconfig.webpack.json"
-      }
-    ]
+        include: path.resolve(__dirname, 'src'),
+        loader: 'ts-loader?configFile=tsconfig.webpack.json',
+      },
+    ],
   },
   resolve: {
-    extensions
+    extensions,
   },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    library: "wallet",
-    libraryTarget: "var"
-  }
-};
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'wallet',
+    libraryTarget: 'var',
+  },
+}
