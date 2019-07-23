47721da67b60575b598d135bd02fd4c058b55b3b
Carson Farmer <carson.farmer@gmail.com>
2019-07-22 21:34:38 -0700

    lint: fix stupid auto-format lint thing from last commit
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- src/__test__/wallet.test.ts
+++ src/__test__/wallet.test.ts
@@ -4,23 +4,23 @@ const mnemonic = 'atom check bronze man connect iron swear degree excess dawn qu
 const expected = [
   {
     index: 0,
-    address: "P6qX6kYu7ntDWYYsGb5beKPCnwZJkzkhPrj4bBrAES6nLAHU",
-    seed: "SVcwoEu2Dz8SWmvDYHqVM14Rwg1BFK8w6xdiqE9JdQukrPho"
+    address: 'P6qX6kYu7ntDWYYsGb5beKPCnwZJkzkhPrj4bBrAES6nLAHU',
+    seed: 'SVcwoEu2Dz8SWmvDYHqVM14Rwg1BFK8w6xdiqE9JdQukrPho'
   },
   {
     index: 1,
-    address: "P5KwuKG6bfCmLDVaJUigr1bjHw6GsgewDNp3G6onBv7Qi3dG",
-    seed: "SU7co5XLJ999uSN7GD5Wwpj3vjEYWa31EevvCpHVCvFBuyDB"
+    address: 'P5KwuKG6bfCmLDVaJUigr1bjHw6GsgewDNp3G6onBv7Qi3dG',
+    seed: 'SU7co5XLJ999uSN7GD5Wwpj3vjEYWa31EevvCpHVCvFBuyDB'
   },
   {
     index: 2,
-    address: "P85WLP28zDhaHBMCtTXmBX3zaW1FWbuNUt7VATxwDWCPXjVQ",
-    seed: "SVuwocyQuBkxMkxvV19gSA97EC4tWdnvbZVisXPVC6kKngYv"
+    address: 'P85WLP28zDhaHBMCtTXmBX3zaW1FWbuNUt7VATxwDWCPXjVQ',
+    seed: 'SVuwocyQuBkxMkxvV19gSA97EC4tWdnvbZVisXPVC6kKngYv'
   },
   {
     index: 3,
-    address: "P98C1XY1amKmNcVjdtZq2E85XfJR7BnTCPZL1fjyC1Gzernz",
-    seed: "SY2734iXMjRGbaFmFGkz7AeiA9LCHZPG16L4mQGCxZVVJuDv"
+    address: 'P98C1XY1amKmNcVjdtZq2E85XfJR7BnTCPZL1fjyC1Gzernz',
+    seed: 'SY2734iXMjRGbaFmFGkz7AeiA9LCHZPG16L4mQGCxZVVJuDv'
   },
   {
     index: [1, 2, 3],
